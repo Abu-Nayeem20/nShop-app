e81c5cfeeb8922074efc265d5957c4a009820908
Abu-Nayeem20 <abu64673@gmail.com>
2021-12-16 23:45:05 +0600

    firebase update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,19 +1,11 @@
-// console.log(process.env)
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   };
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyAgpfPtl2ur_6GpUnNqcX4WdJvs0rIvPLg",
-    authDomain: "nshop0.firebaseapp.com",
-    projectId: "nshop0",
-    storageBucket: "nshop0.appspot.com",
-    messagingSenderId: "272584398324",
-    appId: "1:272584398324:web:bf81fc53b877940ce515a1"
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
+
 export default firebaseConfig;  
